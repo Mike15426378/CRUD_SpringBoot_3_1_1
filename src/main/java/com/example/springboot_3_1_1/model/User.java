a44b7241fa5b5d61d6cf6543603aae8c9dad7351
Mike15426378 / <Ravaga228>
2022-02-27 17:39:50 +0300

    v1 lombok

--- src/main/java/com/example/springboot_3_1_1/model/User.java
+++ src/main/java/com/example/springboot_3_1_1/model/User.java
@@ -22,44 +22,4 @@ public class User {
 
     @Column
     private String email;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
